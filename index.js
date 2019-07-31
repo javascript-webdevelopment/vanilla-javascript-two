bac2cb1d0dc21041b43affab9b1315d4bc5971ab
tstokes9328 <taytestokes9328@gmail.com>
2019-07-30 21:40:04 -0600

    added forms and dom selectors

--- index.js
+++ index.js
@@ -5,3 +5,34 @@ document.getElementById('myInput').addEventListener('change', function(event){
     inputValue = event.target.value
     console.log(inputValue);
 });
+
+// Event Bubbling Example
+const parent = document.querySelector('.parent');
+const child = document.querySelector('.child');
+
+parent.addEventListener('click', function(){
+    console.log('parent was clicked');
+});
+
+// not stopping the bubbling effect
+child.addEventListener('click', function(){
+    console.log('child was clicked');
+});
+
+// stopping the bubbling effect
+child.addEventListener('click', function(event){
+    event.stopPropagation();
+    console.log('child was clicked');
+});
+
+// Form preventDefault Example
+const formBtn = document.getElementById('form-btn');
+
+formBtn.addEventListener('click', function(event){
+    // prevent default event
+    event.preventDefault();
+    // stop event bubbling
+    event.stopPropagation();
+
+    console.log('stopped form submission')
+});
